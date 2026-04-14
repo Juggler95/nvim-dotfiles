eca080fe5c665de49f84c638d6866b8e4da7bb58
Juggler95Linux <fireboltisaac@gmail.com>
2026-04-14 11:25:33 -0700

    made background transparent

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,6 +3,14 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd.colorscheme("catppuccin") -- tokyonight, catppuccin, cyberdream
+-- transpararent background
+vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]]
+
 
 vim.opt.number = true
 vim.wo.relativenumber = true
@@ -10,21 +18,21 @@ vim.opt.clipboard = "unnamedplus"
 
 -- hightlight yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 -- auto format on save
 vim.api.nvim_create_augroup("AutoFormatting", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
-  -- pattern = '*.lua',
-  group = "AutoFormatting",
-  callback = function()
-    vim.lsp.buf.format({ async = true })
-  end,
+	-- pattern = '*.lua',
+	group = "AutoFormatting",
+	callback = function()
+		vim.lsp.buf.format({ async = true })
+	end,
 })
 
 vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#aaaaaa" }) -- Dark grey for numbers above
--- lua/plugins/qol.lua
+++ lua/plugins/qol.lua
@@ -2,9 +2,21 @@ return {
 
 	-- "Surround" ys
 	-- {
-	-- 	"tpope/vim-surround",
+	-- "tpope/vim-surround",
 	-- },
 
+	-- "Surround" ysiw), ysiw(,  ys$", ds], dst remove html tags, cs'", csth1 change tag, dsf
+	{
+		"kylechui/nvim-surround",
+		version = "^4.0.0", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		-- Optional: See `:h nvim-surround.configuration` and `:h nvim-surround.setup` for details
+		-- config = function()
+		--     require("nvim-surround").setup({
+		--         -- Put your configuration here
+		--     })
+		-- end
+	},
 	--{} Autoclose
 	{
 		"m4xshen/autoclose.nvim",
